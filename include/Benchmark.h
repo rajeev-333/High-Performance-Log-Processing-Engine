375ce26e2c7323b5627ae04f87ef4530f23082ee
Rajeev Gupta <rg24eem2r31@student.nitw.ac.in>
2026-03-17 13:44:39 +0530

    Update Benchmark.h

---
+++ include/Benchmark.h
@@ -0,0 +1,18 @@
+#ifndef BENCHMARK_H
+#define BENCHMARK_H
+
+/*
+Runs performance benchmark comparing:
+
+Mutex queue
+Lock-free queue
+*/
+
+class Benchmark
+{
+public:
+
+    static void run();
+};
+
+#endif
